420a42549780be26abdc8c597381bc8f865a9757
Michael Ackerman <binarymac@gmail.com>
2012-10-10 15:37:49 -0700

    added pagination for posts

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,14 +1,21 @@
 from django.conf.urls import patterns, include, url
-from myblog.views import PostsListView, PostDetailView, TaggedPostsListView
+from myblog.views import PostListView, PostDetailView, TaggedPostsListView
 
 
 urlpatterns = patterns('',
-    url(r'^$', PostsListView.as_view(), name='post_list'),
-    url(r'^tag/(?P<tag>[-\w\d]+)/$',
+    url(
+        r'^$', 
+        PostListView.as_view(),
+        name="post_list",
+    ),
+    url(
+        r'^tag/(?P<tag>[-\w\d]+)/$',
         TaggedPostsListView.as_view(), 
-        name='tagged_post_list'),
-    url(r'^(?P<pk>\d+)/(?P<slug>[-\w\d]+)/$',
+        name='tagged_post_list'
+    ),
+    url(
+        r'^(?P<pk>\d+)/(?P<slug>[-\w\d]+)/$',
         PostDetailView.as_view(),
         name='post_detail',
-        ),
+    ),
 )
--- myblog/views.py
+++ myblog/views.py
@@ -2,15 +2,19 @@ from django.views.generic import ListView, DetailView, TemplateView
 from myblog.models import Post
 
 
-class PostsListView(ListView):
+class PostListView(ListView):
     model = Post
+    queryset = Post.objects.all()
+    paginate_by = '2' 
+    context_object_name = 'post_list'
     template_name = 'blog.html'
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog.html"
-    template_object_name = "post"
+    context_object_name = "post"
+
 
 class TaggedPostsListView(TemplateView):
     template_name = 'blog.html'
