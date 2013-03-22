a43de26377102cdcd7b4ee5b1630a31c8f4b1d70
Michael Ackerman <binarymac@gmail.com>
2013-03-22 14:10:23 -0700

    idea view now returns 3 categorized lists

--- creative/views.py
+++ creative/views.py
@@ -1,7 +1,25 @@
-from django.views.generic import ListView
+from django.views.generic import TemplateView
 from .models import Idea
 
 
-class IdeaListView(ListView):
-    model = Idea()
+# dpaste.org/Ik9wA/
+class IdeaListView(TemplateView):
     template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        experimentation_list = Idea.objects.filter(
+            stage__name="Experimentation"
+        )
+        incubation_list = Idea.objects.filter(
+            stage__name="Incubation"
+        )
+        implementation_list = Idea.objects.filter(
+            stage__name="Implementation"
+        )
+
+        context = {
+            'experimentation_list': experimentation_list,
+            'incubation_list': incubation_list,
+            'implementation_list': implementation_list,
+        }
+        return super(IdeaListView, self).get_context_data(**context)
