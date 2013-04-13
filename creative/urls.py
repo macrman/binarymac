b1e2918d938174b8ab0278df759e72f5e9789036
Michael Ackerman <binarymac@gmail.com>
2013-04-13 16:13:00 -0700

    layout finalized, working idea list

--- creative/urls.py
+++ creative/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns
-from .views import IdeaListView
+from .views import IdeaListView, IdeaDetailView
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
+    (r'(?P<stage>\w+)/(?P<pk>\d+)/$', IdeaDetailView.as_view()),
     (r'(?P<stage>\w+)/$', IdeaListView.as_view()),
 )
--- creative/views.py
+++ creative/views.py
@@ -1,21 +1,38 @@
-from django.views.generic import ListView, TemplateView
-from .models import Idea
+from django.views.generic import TemplateView
+from django.views.generic.base import ContextMixin
+from .models import Idea, Stage
 
 
-class IdeaListView(TemplateView):
-    template_name = "creations.html"
-    model = Idea
+class IdeaListMixin(ContextMixin):
 
     def get_context_data(self, **kwargs):
-        mykwargs = self.kwargs['stage']
-        if mykwargs == "all":
-            context = {"idea_list": Idea.objects.all()}
+        stage = self.kwargs.get("stage")
+        idea_list = Idea.objects.order_by('-last_updated')
+        context = {}
+        if stage == "all":
+            stage_detail = Stage()
+            stage_detail.name = "all"
+            stage_detail.info = "blah blah blah... all posts"
         else:
-            context = {
-                "idea_list": Idea.objects.filter(stage__name__iexact=mykwargs)
-            }
-        return super(IdeaListView, self).get_context_data(**context)
+            idea_list = idea_list.filter(stage__name__iexact=stage)
+            stage_detail = Stage.objects.get(name__iexact=stage)
+        context["stage_detail"] = stage_detail
+
+        if self.kwargs.get("pk"):
+            pk = self.kwargs.get("pk")
+            current_idea = Idea.objects.get(pk=pk, stage__name__iexact=stage)
+            idea_list = idea_list.filter(
+                last_updated__lte=current_idea.last_updated
+            )
+            context["current_idea"] = current_idea
+
+        context["idea_list"] = idea_list
+        return super(IdeaListMixin, self).get_context_data(**context)
 
 
-class ExperimentationListView(ListView):
-    pass
+class IdeaListView(IdeaListMixin, TemplateView):
+    template_name = "ideas.html"
+
+
+class IdeaDetailView(IdeaListMixin, TemplateView):
+    template_name = "idea_detail.html"
