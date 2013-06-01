b88972c16aa0dd75aa513b5c318730ad9ffdfce8
Michael Ackerman <binarymac@gmail.com>
2013-05-31 18:04:40 -0700

    contact page now has menu, active

--- binarymac/views.py
+++ binarymac/views.py
@@ -15,6 +15,12 @@ class CreativeMethodView(TemplateView):
 class ContactView(TemplateView):
     template_name = 'contact.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ContactView, self).get_context_data(**kwargs)
+        active = ['/contact', None]
+        context['menu'] = menulist(active)
+        return context
+
 
 class HomeView(TemplateView):
     template_name = 'home.html'
